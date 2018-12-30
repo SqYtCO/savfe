b1fdc1d1592fd13af85e2347b0ffde2f450784ac
SqYtCO <sqyt.co@gmail.com>
2018-12-30 15:48:22 +0100

    new messages

--- messages.h
+++ messages.h
@@ -9,24 +9,28 @@ namespace MSG
 	constexpr const char* UNKNOWN_COMMAND_W_ARG = "Unknown command: ";
 	constexpr const char* FATALE_TERMINAL_ERROR = "\n\nFatale Terminal Error!\nPlease contact the developer!\n\n";
 	constexpr const char* FATALE_RUNNING_ERROR = "\n\nFatale Running Error!\nPlease contact the developer!\n\n";
-	constexpr const char* OPERATION_SUCCESS = "Operation success!";
-	constexpr const char* OPERATION_FAILURE = "Operation failed!";
 	constexpr const char* NO_SUCH_FILE_OR_DIRECTORY_W_ARG = " is neither a file nor a directory.";
 	constexpr const char* FILE_NOT_FOUND_W_ARG = " not found!";
 	constexpr const char* NO_DIRECTORY_W_ARG = " is no valid directory!";
+	constexpr const char* NEEDS_ARGUMENT_W_ARG = " needs an argument!";
 	constexpr const char* NOT_ON_THE_LIST_W_ARG = " is not on the list!";
 	constexpr const char* ALREADY_ON_THE_LIST_W_ARG = " is already on the list!";
 	constexpr const char* ALREADY_ON_THE_LIST_INCLUDED_IN_PARENT_1_W_ARG = " is a subdirectory of ";
 	constexpr const char* ALREADY_ON_THE_LIST_INCLUDED_IN_PARENT_2_W_ARG = " which is already included in the list.";
 	constexpr const char* REMOVE_TO_ADD_REQUEST_ANSWER_1_W_ARG = "Remove ";
-	constexpr const char* REMOVE_TO_ADD_SPACES_FOR_LISTING = "       ";
 	constexpr const char* REMOVE_TO_ADD_REQUEST_ANSWER_2_W_ARG = "to add ";
 	constexpr const char* REMOVE_TO_ADD_REQUEST_ANSWER_3_W_ARG = "?\nThis action can not be undone! (y/n): ";
+	constexpr const char* REMOVE_TO_ADD_SPACES_FOR_LISTING = "       ";
 	constexpr const char* SUCCESSFULLY_REMOVED_W_ARG = " successfully removed!";
 	constexpr const char* SUCCESSFULLY_ADDED_W_ARG = " successfully added!";
 	constexpr const char* OPERATION_CANCELED = "Operation canceled";
 	constexpr const char* CLEAR_LIST_REQUEST_ANSWER = "Clear all directories and files? This action can not be undone! (y/n): ";
 	constexpr const char* SUCCESSFULLY_SET_AS_DESTINAION_W_ARG = " successfully set as destination!";
+	constexpr const char* IS_NO_DIRECTORY_W_ARG = " is no directory!";
+	constexpr const char* NO_CONFIGFILE_FOUND = "No configuration found!";
+	constexpr const char* NO_LOGFILE_FOUND = "No log found!";
+	constexpr const char* NO_LISTFILE_FOUND = "No configuration found!";
+	constexpr const char* IS_NO_VALID_BEHAVIOR_W_ARG = " is no valid behavior code!";
 }
 
 namespace LOG_MSG
